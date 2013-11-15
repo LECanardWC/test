77f96da40e9b16b7d9f359c8f2f4548d519bb9f1
s7venth <sylvain91130@hotmail.fr>
2013-11-15 11:58:42 +0100

    Update Main.java
    j'ai vite fait changer

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	
 	Bddfenetre b1 = new Bddfenetre(connectDB.inserer());
 	System.out.print("Et encore une victoire pour le plow de Canard WC, et merde...");
-	
+		System.out.print("ou pas !!!");
 	}
 
 }
